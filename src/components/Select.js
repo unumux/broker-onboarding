62c06e012e826ba4b53272088046452b95581797
Carl Henderson <chuckhendo@gmail.com>
2017-12-05 13:19:32 -0500

    update select to behave better with default value

--- src/components/Select.js
+++ src/components/Select.js
@@ -7,11 +7,10 @@ const StyledSelect = styled.select`
 
 export class Select extends React.PureComponent {
     render() {
-        const {onChange, selectedValue, options} = this.props;
-
+        const {onChange, selectedValue = "Not set", options} = this.props;
         return (
             <StyledSelect onChange={onChange} value={selectedValue}>
-                <option disabled> -- select an option -- </option>
+                <option disabled value="Not set"> -- select an option -- </option>
                 {options.map((opt) => (
                     <option key={opt} value={opt}>{opt}</option>
                 ))}
