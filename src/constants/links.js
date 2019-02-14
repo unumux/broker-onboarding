b907585473d73445a997f3687a13d4f0b9f4164a
JSpell <mjspell@gmail.com>
2019-02-14 14:13:34 -0500

    Requested update - Changed == to %3D%3D in links.js

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,51 +1,51 @@
 export const links = [
     {
         answers: [true, 'Broker', true, 'SSN', undefined, undefined, undefined, undefined, 'Colonial Life', true],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBBkxv9SZexjQ=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBBkxv9SZexjQ%3D%3D"
     },
     {
         answers: [true, 'Broker', true, 'SSN', undefined, undefined, undefined, undefined, 'Independent Broker', undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBLKyf5AhE8IQ=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBLKyf5AhE8IQ%3D%3D"
     },
     {
         answers: [true, 'Broker', true, 'SSN', undefined, undefined, undefined, undefined, 'NY Life', undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpA51Bnz+VqcKA=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpA51Bnz+VqcKA%3D%3D"
     },
     {
         answers: [true, 'Broker', true, 'SSN', undefined, undefined, undefined, undefined, 'NMO Member', undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpAgqPhYADylog=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpAgqPhYADylog%3D%3D"
     },
     {
         answers: [true, 'Broker', true, 'Tax ID', "Agency, Brokerage Firm, or Company", true, "Colonial Life", true, undefined, undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpCfQ3TIuPD1bQ=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpCfQ3TIuPD1bQ%3D%3D"
     },
     {
         answers: [true, 'Broker', true, 'Tax ID', "Agency, Brokerage Firm, or Company", true, "Independent Company", undefined, undefined, undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBWil9IfBnaHg=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBWil9IfBnaHg%3D%3D"
     },
     {
         answers: [true, 'Broker', true, 'Tax ID', "Agency, Brokerage Firm, or Company", true, "NMO Member", undefined, undefined, undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpD5aJX/ccyrjg=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpD5aJX/ccyrjg%3D%3D"
     },
     {
         answers: [true, 'Broker', true, 'Tax ID', "Individual Broker", true, "Independent Company", undefined, undefined, undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBZ+XSBXEvejw=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBZ+XSBXEvejw%3D%3D"
     },
     {
         answers: [true, 'Broker', true, 'Tax ID', "Individual Broker", true, "NMO", undefined, undefined, undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBHEk23uXbpZQ=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBHEk23uXbpZQ%3D%3D"
     },
     {
         answers: [true, 'Benefit Counselor (Enroller)', true, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpAOeDIhO4tHyw=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpAOeDIhO4tHyw%3D%3D"
     },
     {
         answers: [true, 'Enrollment Firm', true, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBWil9IfBnaHg=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpBWil9IfBnaHg%3D%3D"
     },
     // If the user answered question 2 with either Third Party Administrator, Benefit Administrator, or Technology Provider, they will be routed to the 'Shared Link' below.
     {
         answers: [true, 'Shared Link', true, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
-        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpAjLeC1MkrGRg=="
+        link: "https://pangea.geninfo.com/Unum/Apply/Default.aspx?BY29ChRPFpAjLeC1MkrGRg%3D%3D"
     },
 ]
